8b06029ad5adbe96abceb2b16b63387501f71801
Achmad Rifqi Rosadi <achmadrifqi09@gmail.com>
2024-09-26 22:46:35 +0700

    update structure module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,16 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { UserModule } from './modules/user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AppMiddleware } from './middlewares/app/app.middleware';
 import { APP_FILTER } from '@nestjs/core';
 import { HttpExceptionFilter } from './filters/http-exception/http-exception.filter';
 import { ConfigModule } from '@nestjs/config';
+import { EmployeeModule } from './modules/employee/employee.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule, PrismaModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule, PrismaModule, EmployeeModule],
   controllers: [AppController],
   providers: [
     {
