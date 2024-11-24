242ee39a67695f07e7cff763166c864538e58c35
Achmad Rifqi Rosadi <achmadrifqi09@gmail.com>
2024-11-24 22:35:31 +0700

    refactor:registration response

--- src/modules/registration/const/registration.const.ts
+++ src/modules/registration/const/registration.const.ts
@@ -3,6 +3,7 @@ import { Prisma } from '@prisma/client';
 export const registrationSelect: Prisma.RegistrationSelect = {
   id: true,
   kode_rm: true,
+  nomor_registrasi: true,
   status_bpjs: true,
   tgl_daftar: true,
   antrian: {
