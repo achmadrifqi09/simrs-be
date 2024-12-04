def892d8eb499ba60b88c133a9575b475fbb3a5e
Achmad Rifqi Rosadi <achmadrifqi09@gmail.com>
2024-12-04 18:57:13 +0700

    fix:vclaim

--- src/modules/registration/validation/registration.validation.ts
+++ src/modules/registration/validation/registration.validation.ts
@@ -11,3 +11,49 @@ export const cancellationValidation = z.object({
   ),
   keterangan_batal: z.string().min(4, 'Keterangan batal minimal 4 karakter'),
 });
+
+export const registrationValidation = z.object({
+  cob: z.number().nullish(),
+  no_cob: z.string().nullish(),
+  nama_wali: z
+    .string()
+    .max(50, { message: 'Nama wali tidak boleh lebih dari 50 karakter' })
+    .nullish(),
+  telp_wali: z
+    .string()
+    .max(50, { message: 'Telepon wali tidak boleh lebih dari 50 karakter' })
+    .nullish(),
+  id_hub_wali: z.string().nullish(),
+  asal_rujukan: z
+    .string()
+    .max(100, { message: 'Asal rujukan tidak boleh lebih dari 50 karakter' })
+    .nullish(),
+  nama_perujuk: z
+    .string()
+    .max(100, { message: 'Nama perujuk tidak boleh lebih dari 50 karakter' })
+    .nullish(),
+  ket_rujukan: z
+    .string()
+    .max(100, { message: 'Nama perujuk tidak boleh lebih dari 50 karakter' })
+    .nullish(),
+});
+
+/*
+ * cob
+ * no asuransi cob
+ * nama_wali
+ * telp_wali
+ * id_hub_wali
+ * asal_rujukan
+ * nama_perujuk
+ * ket_rujukan
+
+ * BPJS
+ * status rujukan
+ * nomor_rujuk_balik
+ * tgl rujukan
+
+ * UMUM
+ * penjamin
+ * no asuransi
+ * */
